d36b4cafaa8dd175ec95fc9bed011c4c197270ba
sanghyup <lkh6667@naver.com>
2023-07-14 17:20:20 +0900

    app1 commit

---
+++ prj1/app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+df = pd.DataFrame({'col1': [1,2,3]})
+df  # 👈 Draw the dataframe
+
+x = 10
+'x', x  # 👈 Draw the string 'x' and then the value of x
+
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+
+st.write(df)
+st.write('Hello world')
+
+st.text_input("Your name", key="name")
+
+st.session_state.name
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+
+
+
+# You can access the value at any point with:
+st.session_state.name
+
+
+
+
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig  # 👈 Draw a Matplotlib chart
